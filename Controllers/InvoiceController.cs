25f7625e395d60b3a5a7ce89678799fbc7914e8a
Natarajan <nataraj.itsolution@gmail.com>
2022-11-19 23:08:14 +0530

    multiple page pdf

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -56,74 +56,79 @@ public class InvoiceController : ControllerBase
     public async Task<IActionResult> GeneratePDF(string InvoiceNo)
     {
         var document = new PdfDocument();
-       // string HtmlContent = "<h1>Welcome to Nihira Techiees</h1>";
-       InvoiceHeader header= await this._container.GetAllInvoiceHeaderbyCode(InvoiceNo);
-        List<InvoiceDetail> detail = await this._container.GetAllInvoiceDetailbyCode(InvoiceNo);
-        string htmlcontent = "<div style='width:100%; text-align:center'>";
-        htmlcontent += "<h2>Welcome to Nihira Techiees</h2>";
-
-        if (header != null)
+        string[] copies = { "Customer copy", "Comapny Copy" };
+        for (int i = 0; i < copies.Length; i++)
         {
-            htmlcontent += "<h2> Invoice No:"+header.InvoiceNo+" & Invoice Date:"+header.InvoiceDate+"</h2>";
-            htmlcontent += "<h3> Customer : "+header.CustomerName+"</h3>";
-            htmlcontent += "<p>"+header.DeliveryAddress+"</p>";
-            htmlcontent += "<h3> Contact : 9898989898 & Email :ts@in.com </h3>";
-            htmlcontent += "<div>";
-        }
+            InvoiceHeader header = await this._container.GetAllInvoiceHeaderbyCode(InvoiceNo);
+            List<InvoiceDetail> detail = await this._container.GetAllInvoiceDetailbyCode(InvoiceNo);
+            string htmlcontent = "<div style='width:100%; text-align:center'>";
+            htmlcontent += "<h2>" + copies[i] + "</h2>";
+            htmlcontent += "<h2>Welcome to Nihira Techiees</h2>";
 
-       
-       
-        htmlcontent += "<table style ='width:100%; border: 1px solid #000'>";
-        htmlcontent += "<thead style='font-weight:bold'>";
-        htmlcontent += "<tr>";
-        htmlcontent += "<td style='border:1px solid #000'> Product Code </td>";
-        htmlcontent += "<td style='border:1px solid #000'> Description </td>";
-        htmlcontent += "<td style='border:1px solid #000'>Qty</td>";
-        htmlcontent += "<td style='border:1px solid #000'>Price</td >";
-        htmlcontent += "<td style='border:1px solid #000'>Total</td>";
-        htmlcontent += "</tr>";
-        htmlcontent += "</thead >";
+            if (header != null)
+            {
+                htmlcontent += "<h2> Invoice No:" + header.InvoiceNo + " & Invoice Date:" + header.InvoiceDate + "</h2>";
+                htmlcontent += "<h3> Customer : " + header.CustomerName + "</h3>";
+                htmlcontent += "<p>" + header.DeliveryAddress + "</p>";
+                htmlcontent += "<h3> Contact : 9898989898 & Email :ts@in.com </h3>";
+                htmlcontent += "<div>";
+            }
 
-        htmlcontent += "<tbody>";
-        if (detail != null && detail.Count > 0)
-        {
-            detail.ForEach(item =>
+
+
+            htmlcontent += "<table style ='width:100%; border: 1px solid #000'>";
+            htmlcontent += "<thead style='font-weight:bold'>";
+            htmlcontent += "<tr>";
+            htmlcontent += "<td style='border:1px solid #000'> Product Code </td>";
+            htmlcontent += "<td style='border:1px solid #000'> Description </td>";
+            htmlcontent += "<td style='border:1px solid #000'>Qty</td>";
+            htmlcontent += "<td style='border:1px solid #000'>Price</td >";
+            htmlcontent += "<td style='border:1px solid #000'>Total</td>";
+            htmlcontent += "</tr>";
+            htmlcontent += "</thead >";
+
+            htmlcontent += "<tbody>";
+            if (detail != null && detail.Count > 0)
+            {
+                detail.ForEach(item =>
+                {
+                    htmlcontent += "<tr>";
+                    htmlcontent += "<td>" + item.ProductCode + "</td>";
+                    htmlcontent += "<td>" + item.ProductName + "</td>";
+                    htmlcontent += "<td>" + item.Qty + "</td >";
+                    htmlcontent += "<td>" + item.SalesPrice + "</td>";
+                    htmlcontent += "<td> " + item.Total + "</td >";
+                    htmlcontent += "</tr>";
+                });
+            }
+            htmlcontent += "</tbody>";
+
+            htmlcontent += "</table>";
+            htmlcontent += "</div>";
+
+            htmlcontent += "<div style='text-align:right'>";
+            htmlcontent += "<h1> Summary Info </h1>";
+            htmlcontent += "<table style='border:1px solid #000;float:right' >";
+            htmlcontent += "<tr>";
+            htmlcontent += "<td style='border:1px solid #000'> Summary Total </td>";
+            htmlcontent += "<td style='border:1px solid #000'> Summary Tax </td>";
+            htmlcontent += "<td style='border:1px solid #000'> Summary NetTotal </td>";
+            htmlcontent += "</tr>";
+            if (header != null)
             {
                 htmlcontent += "<tr>";
-                htmlcontent += "<td>" + item.ProductCode + "</td>";
-                htmlcontent += "<td>" + item.ProductName + "</td>";
-                htmlcontent += "<td>" + item.Qty + "</td >";
-                htmlcontent += "<td>" + item.SalesPrice + "</td>";
-                htmlcontent += "<td> " + item.Total + "</td >";
+                htmlcontent += "<td style='border: 1px solid #000'> " + header.Total + " </td>";
+                htmlcontent += "<td style='border: 1px solid #000'>" + header.Tax + "</td>";
+                htmlcontent += "<td style='border: 1px solid #000'> " + header.NetTotal + "</td>";
                 htmlcontent += "</tr>";
-            });
+            }
+            htmlcontent += "</table>";
+            htmlcontent += "</div>";
+
+            htmlcontent += "</div>";
+
+            PdfGenerator.AddPdfPages(document, htmlcontent, PageSize.A4);
         }
-        htmlcontent += "</tbody>";
-
-        htmlcontent += "</table>";
-        htmlcontent += "</div>";
-
-        htmlcontent += "<div style='text-align:right'>";
-        htmlcontent += "<h1> Summary Info </h1>";
-        htmlcontent += "<table style='border:1px solid #000;float:right' >";
-        htmlcontent += "<tr>";
-        htmlcontent += "<td style='border:1px solid #000'> Summary Total </td>";
-        htmlcontent += "<td style='border:1px solid #000'> Summary Tax </td>";
-        htmlcontent += "<td style='border:1px solid #000'> Summary NetTotal </td>";
-        htmlcontent += "</tr>";
-        if (header != null)
-        {
-            htmlcontent += "<tr>";
-            htmlcontent += "<td style='border: 1px solid #000'> "+header.Total+" </td>";
-            htmlcontent += "<td style='border: 1px solid #000'>" + header.Tax + "</td>";
-            htmlcontent += "<td style='border: 1px solid #000'> " + header.NetTotal + "</td>";
-            htmlcontent += "</tr>";
-        }
-        htmlcontent += "</table>";
-        htmlcontent += "</div>";
-
-        htmlcontent += "</div>";
-        PdfGenerator.AddPdfPages(document, htmlcontent, PageSize.A4);
         byte[]? response = null;
         using(MemoryStream ms = new MemoryStream())
         {
@@ -134,7 +139,44 @@ public class InvoiceController : ControllerBase
         return File(response, "application/pdf", Filename);
     }
 
-   
 
+    [HttpGet("generateaddresspdf")]
+    public async Task<IActionResult> generateaddresspdf()
+    {
+        var document = new PdfDocument();
+        List<InvoiceHeader> invoicelist = await this._container.GetAllInvoiceHeader();
+        int processcount = 0;
+        int breakcount = 0;
+        string htmlcontent = String.Empty;
+        invoicelist.ForEach(item =>
+        {
+            htmlcontent += "<div style='width:100%;padding:5px;margin:5px;border:1px solid #ccc'>";
+            htmlcontent += "<h1>"+item.CustomerName+" [<b>"+item.InvoiceNo+"</b>]</h1>";
+            htmlcontent += "<h2>"+item.DeliveryAddress+"</h2>";
+            htmlcontent += "<h2>Payable Amount" + item.NetTotal + "</h2>";
+            htmlcontent += "</div>";
+            processcount++;
+            breakcount++;
+            if (breakcount == 4)
+            {
+               
+                PdfGenerator.AddPdfPages(document, htmlcontent, PageSize.A4);
+                breakcount = 0;
+                htmlcontent = String.Empty;
+            }
+            else if (processcount == invoicelist.Count)
+            {
+                PdfGenerator.AddPdfPages(document, htmlcontent, PageSize.A4);
+            }
+        });
+        byte[]? response = null;
+        using (MemoryStream ms = new MemoryStream())
+        {
+            document.Save(ms);
+            response = ms.ToArray();
+        }
+        string Filename = "Invoice_Address.pdf";
+        return File(response, "application/pdf", Filename);
+    }
 
 }
