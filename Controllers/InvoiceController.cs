3d1fa538e74f0850d6c1bcb7979c92d81e0f53aa
Natarajan <nataraj.itsolution@gmail.com>
2022-11-24 20:12:16 +0530

    Insert image in pdf

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -13,9 +13,11 @@ namespace SalesOrderAPI.Controllers;
 public class InvoiceController : ControllerBase
 {
     private readonly IInvoiceContainer _container;
-    public InvoiceController(IInvoiceContainer container)
+    private readonly IWebHostEnvironment environment;
+    public InvoiceController(IInvoiceContainer container,IWebHostEnvironment webHostEnvironment)
     {
         this._container = container;
+        this.environment = webHostEnvironment;
     }
 
     [HttpGet("GetAllHeader")]
@@ -56,14 +58,19 @@ public class InvoiceController : ControllerBase
     public async Task<IActionResult> GeneratePDF(string InvoiceNo)
     {
         var document = new PdfDocument();
+        string imgeurl = "data:image/png;base64, " + Getbase64string() + "";
+
         string[] copies = { "Customer copy", "Comapny Copy" };
         for (int i = 0; i < copies.Length; i++)
         {
             InvoiceHeader header = await this._container.GetAllInvoiceHeaderbyCode(InvoiceNo);
             List<InvoiceDetail> detail = await this._container.GetAllInvoiceDetailbyCode(InvoiceNo);
             string htmlcontent = "<div style='width:100%; text-align:center'>";
+            htmlcontent += "<img style='width:80px;height:80%' src='" + imgeurl + "'   />";
             htmlcontent += "<h2>" + copies[i] + "</h2>";
             htmlcontent += "<h2>Welcome to Nihira Techiees</h2>";
+
+            
 
             if (header != null)
             {
@@ -177,6 +184,37 @@ public class InvoiceController : ControllerBase
         }
         string Filename = "Invoice_Address.pdf";
         return File(response, "application/pdf", Filename);
+    }
+
+    [HttpGet("GenPDFwithImage")]
+    public async Task<IActionResult> GenPDFwithImage()
+    {
+        var document = new PdfDocument();
+        string htmlelement = "<div style='width:100%'>";
+        // string imgeurl = "https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg";
+        //string imgeurl = "https://" + HttpContext.Request.Host.Value + "/Uploads/common/logo.jpeg";
+        string imgeurl = "data:image/png;base64, " + Getbase64string() + "";
+        htmlelement += "<img style='width:80px;height:80%' src='" + imgeurl + "'   />";
+        htmlelement += "<h2>Welcome to Nihira Techiees</h2>";
+        htmlelement += "</div>";
+        PdfGenerator.AddPdfPages(document,htmlelement, PageSize.A4);
+        byte[] response = null;
+        using(MemoryStream ms = new MemoryStream())
+        {
+            document.Save(ms);
+            response=ms.ToArray();
+        }
+        return File(response, "application/pdf", "PDFwithImage.pdf");
+
+    }
+
+    [NonAction]
+    public string Getbase64string()
+    {
+        string filepath = this.environment.WebRootPath + "\\Uploads\\common\\logo.jpeg";
+        byte[] imgarray=System.IO.File.ReadAllBytes(filepath);
+        string base64 = Convert.ToBase64String(imgarray);
+        return base64;
     }
 
 }
